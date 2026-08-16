4c1d41acbd1d8735631e8ce74fa5f8ed6aabee67
kit8379 <manhhkit@gmail.com>
2026-08-16 16:12:07 +0800

    Use modern Jedis ConnectionPool API

--- src/main/java/org/me/synccommand/shared/redis/RedisHandler.java
+++ src/main/java/org/me/synccommand/shared/redis/RedisHandler.java
@@ -1,25 +1,34 @@
 package org.me.synccommand.shared.redis;
 
+import redis.clients.jedis.Connection;
+import redis.clients.jedis.ConnectionPool;
+import redis.clients.jedis.ConnectionPoolConfig;
+import redis.clients.jedis.DefaultJedisClientConfig;
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisClientConfig;
 import redis.clients.jedis.JedisPubSub;
 
 public class RedisHandler {
-    private static JedisPool pool;
+    private static ConnectionPool pool;
 
     public static void connect(String host, int port, String password) {
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        ConnectionPoolConfig poolConfig = new ConnectionPoolConfig();
+
+        DefaultJedisClientConfig.Builder clientConfigBuilder = DefaultJedisClientConfig.builder()
+                .connectionTimeoutMillis(2000)
+                .socketTimeoutMillis(2000);
 
         if (password != null && !password.isEmpty()) {
-            pool = new JedisPool(poolConfig, host, port, 2000, password);
-        } else {
-            pool = new JedisPool(poolConfig, host, port, 2000);
+            clientConfigBuilder.password(password);
         }
+
+        JedisClientConfig clientConfig = clientConfigBuilder.build();
+        pool = new ConnectionPool(new HostAndPort(host, port), clientConfig, poolConfig);
     }
 
     public static void publish(String channel, String message) {
-        try (Jedis jedis = pool.getResource()) {
+        try (Jedis jedis = getJedis()) {
             jedis.publish(channel, message);
         } catch (Exception e) {
             e.printStackTrace();
@@ -27,11 +36,16 @@ public class RedisHandler {
     }
 
     public static void subscribe(JedisPubSub pubSub, String... channels) {
-        try (Jedis jedis = pool.getResource()) {
+        try (Jedis jedis = getJedis()) {
             jedis.subscribe(pubSub, channels);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static Jedis getJedis() {
+        Connection connection = pool.getResource();
+        return new Jedis(connection);
     }
 
     public static void disconnect() {
